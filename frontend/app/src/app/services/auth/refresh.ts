02f9601e56f4917314aa4cf7f37694532e84f7e3
Isaías Ramos <isaias.ramos.olive@gmail.com>
2023-11-16 14:54:42 -0300

    project done

---
+++ frontend/app/src/app/services/auth/refresh.ts
@@ -0,0 +1,19 @@
+"use server"
+
+export async function refresh(token: string) {
+    const res = await fetch("http://localhost:4000/api/refresh", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ refresh_token: token }),
+        next: {
+            revalidate: 5
+        }
+    })
+        .then((res) => {
+            return res.json();
+        })
+        .catch((err) => console.log("Error"));
+    return res;
+}
